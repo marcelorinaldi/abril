ecc2e478b413fb034bb0b6b8322ffd72c33f1c17
Marcelo Rinaldi <143459591+marcelorinaldi@users.noreply.github.com>
2024-04-02 11:27:45 -0300

    variaveis javascript

--- TelaInicial.js
+++ TelaInicial.js
@@ -3,9 +3,28 @@ import styles from './estilo/estilo.js';
 import Menu from './components/menu.js';
 
 function TelaInicial({ navigation }) {
+  // var b = 10;
+  // var b = 'teste';
+  // const a = 2;
+  // let c = 4;
+  // soma = a+b+c
+  // console.log(soma);
+
+  var a = 10;
+  var b = 5;
+  var c = '';
+  c = a + b;
+
+  if(c > 10) {
+    console.log('maior');
+    c = c +' maior';
+  } else {
+    console.log('menor');
+    c = c + ' menor';
+  }
   return (
     <><View style={styles.container}>
-      <Text>Clique abaixo para Tela desejada</Text>
+      <Text>Clique abaixo para Tela desejada - {c}</Text>
       <Button title="Avançar Tela2" onPress={() => navigation.navigate('Tela2')} />
       <Button title="Avançar Tela3" onPress={() => navigation.navigate('Tela3')} />
 
